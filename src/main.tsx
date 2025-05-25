451b5bd7b30c2b156b8ad07e290c370cfd6057a1
Linh <linhpham0519@gmail.com>
2025-05-25 15:54:39 -0400

    implemented fetching api using useQuery

--- src/main.tsx
+++ src/main.tsx
@@ -10,7 +10,7 @@ ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 ).render(
   <React.StrictMode>
-    <QueryClientProvider>
+    <QueryClientProvider client={queryClient}>
     <App />
     </QueryClientProvider>
 
--- src/react-query/TodoList.tsx
+++ src/react-query/TodoList.tsx
@@ -9,27 +9,44 @@ interface Todo {
   completed: boolean;
 }
 
-const TodoList = () => {
-  // useQuery({
-  //   // key is used to identify the cache
-  //   queryKey: ['todos'],
-  //   queryFn: 
-  // })
-  const [todos, setTodos] = useState<Todo[]>([]);
-  const [error, setError] = useState('');
+const TodoList = ()=> {
+  const fetchTodos = async (): Promise<Todo[]> => {
+    const res = await fetch('https://jsonplaceholder.typicode.com/todos');
+    if (!res.ok) {
+      throw new Error(`Failed to fetch todos: ${res.statusText}`);
+    }
+    const data = await res.json();
+    return data as Todo[];
+  };
 
-  useEffect(() => {
-    axios
-      .get('https://jsonplaceholder.typicode.com/todos')
-      .then((res) => setTodos(res.data))
-      .catch((error) => setError(error));
-  }, []);
+  // const fetchTodos = () => 
+  //   axios
+  //     .get<Todo[]>('https://jsonplaceholder.typicode.com/todos')
+  //     .then((res) => 
+  //      res.data);
+  // query gets back an object with properties like data, error, isLoading, etc.
+  // we get autore fetch, auto retry and caching
+  const {data: todos, error} = useQuery<Todo[], Error>({
+    // key is used to identify the cache internally
+    queryKey: ['todos'],
+    queryFn: fetchTodos,
+  })
 
-  if (error) return <p>{error}</p>;
+  // const [todos, setTodos] = useState<Todo[]>([]);
+  // const [error, setError] = useState('');
+
+  // useEffect(() => {
+  //   axios
+  //     .get('https://jsonplaceholder.typicode.com/todos')
+  //     .then((res) => setTodos(res.data))
+  //     .catch((error) => setError(error));
+  // }, []);
+
+  if (error) return <p>{error.message}</p>;
 
   return (
     <ul className="list-group">
-      {todos.map((todo) => (
+      {todos?.map((todo) => (
         <li key={todo.id} className="list-group-item">
           {todo.title}
         </li>
